681835b8224aeac1c0dffbc55cc29d17ae8d63e0
Isainjimenez <61337276+Isainjimenez@users.noreply.github.com>
2024-06-11 15:06:06 -0600

    corrección1  Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import ph from '../img/photo.jpg';
 
 const Home = () => {
   return (
-    <div className="card p-4 text-center ">
+    <div className="card p-4 text-center">
       <h2>Isain Jimenez Martinez</h2>
-      <div className="flex flex-col items-center ">
-        <img src="src/img/photo.jpg" alt="Desarrollador" className="w-32 h-32 rounded-full mb-5" />
-        <p className="description">
-          Soy Isain, un estudiante de programacion apasionado en desarrollo de software. 
-          <p>He trabajado en una variedad de proyectos que abarcan desde aplicaciones web hasta sistemas embebidos.  
-          <p> Mis principales lenguajes de programación incluyen Java, Python, Matlab, C++, tengo trabajos en React en javascript y tambien he trabajado proyactos en Arduino y un poco de experiencia en redes. 
-          <p>Disfruto resolver problemas complejos y siempre estoy buscando nuevos desafíos que me permitan mejorar mis habilidades y aprender nuevas tecnologías. 
-          <p>En mi tiempo libre, me gusta jugar videojuegos y documentarme sobrte temas que aun desconozco.
-        </p></p></p></p></p>
+      <div className="flex flex-col items-center">
+        <img src={ph} alt="Desarrollador" className="w-32 h-32 rounded-full mb-5" />
+        <div className="description text-left">
+          <p>Soy Isain, un estudiante de programación apasionado en desarrollo de software.</p>
+          <p>He trabajado en una variedad de proyectos que abarcan desde aplicaciones web hasta sistemas embebidos.</p>
+          <p>Mis principales lenguajes de programación incluyen Java, Python, Matlab, C++, tengo trabajos en React en JavaScript y también he trabajado en proyectos con Arduino y tengo un poco de experiencia en redes.</p>
+          <p>Disfruto resolver problemas complejos y siempre estoy buscando nuevos desafíos que me permitan mejorar mis habilidades y aprender nuevas tecnologías.</p>
+          <p>En mi tiempo libre, me gusta jugar videojuegos y documentarme sobre temas que aún desconozco.</p>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Home;
-
